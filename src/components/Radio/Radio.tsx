282612d911b30f2876b00bd4187c339887280501
Alexandr Petyaykin <a.petyaykin@infomaximum.com>
2023-01-20 15:11:34 +0300

    feat: перенес Radio [IS-316]

---
+++ src/components/Radio/Radio.tsx
@@ -0,0 +1,21 @@
+import { Radio as AntRadio } from "antd";
+import { PureComponent } from "react";
+import { radioButtonStyle } from "./Radio.style";
+import type { IRadioProps } from "./Radio.types";
+
+const { Group, Button } = AntRadio;
+
+class Radio extends PureComponent<IRadioProps> {
+  public static Button = Button;
+  public static Group = Group;
+
+  public override render() {
+    return (
+      <span test-id={this.props["test-id"]}>
+        <AntRadio {...this.props} css={radioButtonStyle} />
+      </span>
+    );
+  }
+}
+
+export default Radio;
