afb6779034da80b7fbfca5f61357d6b8702e0a35
Alexandr Petyaykin <a.petyaykin@infomaximum.com>
2023-01-20 14:58:01 +0300

    feat: перенес ErrorBoundary [IS-316]

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,75 @@
+import withModalError from "@im/base/src/decorators/hocs/withModalError/withModalError";
+import type { ComponentType, ErrorInfo } from "react";
+import { Component } from "react";
+import { ErrorHandling } from "@im/utils";
+import type {
+  IAppErrorBoundaryProps,
+  IErrorBoundaryProps,
+  IErrorBoundaryState,
+} from "./ErrorBoundary.types";
+
+const AppErrorHandlingService = new ErrorHandling();
+
+class ErrorBoundary extends Component<IErrorBoundaryProps, IErrorBoundaryState> {
+  public static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  public override readonly state = {
+    hasError: false,
+  };
+
+  public override componentDidUpdate(prevProps: IErrorBoundaryProps) {
+    const { location } = this.props;
+    const { location: prevLocation } = prevProps;
+    const { hasError } = this.state;
+
+    if (hasError && prevLocation?.pathname !== location?.pathname) {
+      this.setState({ hasError: false });
+    }
+  }
+
+  public override componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    const { code, showModalError, extraParams } = this.props;
+
+    if (process.env.NODE_ENV === "production") {
+      const preparedError = {
+        code,
+        name: error.name,
+        message: error.message,
+        path: this.getPathName(),
+        ...extraParams,
+      };
+
+      AppErrorHandlingService.registerError({ ...preparedError, errorInfo });
+
+      showModalError({
+        message: JSON.stringify(preparedError),
+      });
+    } else {
+      // eslint-disable-next-line no-console
+      console.error(
+        `ошибка произошла по пути ${this.getPathName()} ошибка ${error} код: ${this.props.code}`
+      );
+    }
+  }
+
+  public getPathName() {
+    return this.props?.location?.pathname ?? window.location?.pathname ?? "app";
+  }
+
+  public override render() {
+    if (this.state.hasError) {
+      // тут нужно отображать заглушку об ошибке
+      return null;
+    }
+
+    return this.props.children;
+  }
+}
+
+export const AppErrorBoundary = withModalError(ErrorBoundary) as unknown as ComponentType<
+  Omit<IAppErrorBoundaryProps, "showModalError">
+>;
+
+export default withModalError(ErrorBoundary);
---
+++ src/components/ErrorBoundary/ErrorBoundary.types.ts
@@ -0,0 +1,23 @@
+import type { NCore } from "@im/core";
+import type { IWithModalErrorProps } from "@im/base/src/decorators/hocs/withModalError/withModalError.types";
+
+export interface IAppErrorBoundaryProps extends IWithModalErrorProps {
+  /**
+   * Дополнительные параметры которые будут обработаны
+   */
+  extraParams?: TDictionary;
+  /**
+   * Код, который говорит какой ErrorBoundary перехватил ошибку
+   */
+  code: string;
+
+  children: React.ReactNode;
+}
+
+export interface IErrorBoundaryProps
+  extends Partial<NCore.TRouteComponentProps>,
+    IAppErrorBoundaryProps {}
+
+export interface IErrorBoundaryState {
+  hasError?: boolean;
+}
