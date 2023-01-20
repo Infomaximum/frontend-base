1d674c4866e40f4a72717aef3a348b13320a96b8
Alexandr Petyaykin <a.petyaykin@infomaximum.com>
2023-01-20 15:19:12 +0300

    feat: перенес EllipsisTooltip [IS-316]

---
+++ src/components/EllipsisTooltip/EllipsisTooltip.tsx
@@ -0,0 +1,38 @@
+import { FC, memo, useMemo, cloneElement, isValidElement } from "react";
+import { ellipsisTextWrapper, ellipsisStyleForSafari } from "./EllipsisTooltip.styles";
+import type { IEllipsisTooltipProps } from "./EllipsisTooltip.types";
+import Tooltip from "@im/base/src/components/Tooltip/Tooltip";
+import { EUserAgents, userAgent } from "@im/utils";
+
+const isSafari = userAgent() === EUserAgents.Safari;
+
+const EllipsisTooltip: FC<IEllipsisTooltipProps> = ({
+  children,
+  title,
+  placement,
+  customTextWrapperStyle,
+  ...rest
+}) => {
+  const css = [customTextWrapperStyle, isSafari ? ellipsisStyleForSafari : null];
+  const clearTitle = useMemo(() => {
+    /** Если это элемент, то нужно убрать некоторые стили которые были добавлены */
+    if (isValidElement(title)) {
+      return cloneElement(title, {
+        css: null,
+        style: null,
+      });
+    }
+
+    return title;
+  }, [title]);
+
+  return (
+    <div style={ellipsisTextWrapper} css={css}>
+      <Tooltip title={clearTitle ?? children} placement={placement} {...rest}>
+        {children}
+      </Tooltip>
+    </div>
+  );
+};
+
+export default memo(EllipsisTooltip);
