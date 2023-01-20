de496a2bb550b59e17ac1f1cf726c40e08ba6345
Alexandr Petyaykin <a.petyaykin@infomaximum.com>
2023-01-20 10:00:56 +0300

    feat: перенес из платформы Alert [IS-316]

---
+++ src/components/Alert/Alert.styles.ts
@@ -0,0 +1,89 @@
+export const alertClassnames = {
+  closeIcon: ".anticon-close",
+  closeButton: ".ant-alert-close-icon",
+  alertIcon: ".ant-alert-icon",
+  alertMessage: ".ant-alert-message",
+};
+
+export const alertStyle = (theme: TTheme) =>
+  ({
+    alignItems: "flex-start",
+    [`${alertClassnames.closeIcon}`]: {
+      color: theme.grey6Color,
+    },
+    [`${alertClassnames.alertMessage}`]: {
+      marginBottom: 0,
+      fontSize: "14px",
+      color: theme.grey8Color,
+    },
+    padding: "9px 16px 9px 15px",
+    display: "flex",
+    [`${alertClassnames.alertIcon}`]: {
+      lineHeight: "unset",
+      position: "relative",
+      top: "2px",
+      fontSize: "14px",
+      marginRight: "9px",
+    },
+  } as const);
+
+export const boldTitleStyle = () => ({
+  fontWeight: "bold",
+});
+
+export const alertDescriptionStyle = (theme: TTheme) => ({
+  color: theme.grey8Color,
+});
+
+export const infoAlertStyle = (theme: TTheme) => ({
+  background: theme.blue1Color,
+  border: `1px solid ${theme.blue3Color}`,
+  [`${alertClassnames.alertIcon}`]: {
+    color: theme.blue6Color,
+  },
+});
+
+export const warningAlertStyle = (theme: TTheme) => ({
+  background: theme.gold1Color,
+  border: `1px solid ${theme.gold3Color}`,
+  [`${alertClassnames.alertIcon}`]: {
+    color: theme.gold6Color,
+  },
+});
+
+export const errorAlertStyle = (theme: TTheme) => ({
+  background: theme.red1Color,
+  border: `1px solid ${theme.red3Color}`,
+  [`${alertClassnames.alertIcon}`]: {
+    color: theme.red6Color,
+  },
+});
+
+export const successAlertStyle = (theme: TTheme) => ({
+  background: theme.green1Color,
+  border: `1px solid ${theme.green3Color}`,
+  [`${alertClassnames.alertIcon}`]: {
+    color: theme.green6Color,
+  },
+});
+
+export const alertBannerStyle = (theme: TTheme) =>
+  ({
+    border: "none",
+    alignItems: "flex-start",
+    [`${alertClassnames.alertIcon}`]: {
+      display: "flex",
+      fontSize: theme.h3FontSize,
+      width: "20px",
+      height: "20px",
+      alignItems: "center",
+    },
+    [`${alertClassnames.closeIcon}`]: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "16px",
+      height: "16px",
+      marginTop: "3px",
+    },
+  } as const);
---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,89 @@
+import React, { useMemo } from "react";
+import { Alert as AntAlert } from "antd";
+import type { IAlertProps } from "./Alert.types";
+import {
+  infoAlertStyle,
+  warningAlertStyle,
+  errorAlertStyle,
+  successAlertStyle,
+  boldTitleStyle,
+  alertDescriptionStyle,
+  alertStyle,
+  alertBannerStyle,
+} from "./Alert.styles";
+import type { Interpolation } from "@emotion/react";
+import Icon, {
+  CloseCircleFilled,
+  CheckCircleFilled,
+  ExclamationCircleFilled,
+} from "@im/base/src/components/Icons/Icons";
+
+const Alert: React.FC<IAlertProps> = (props) => {
+  const icon = useMemo(() => {
+    let iconComponent: React.FC;
+    switch (props.type) {
+      case "success":
+        iconComponent = CheckCircleFilled;
+        break;
+      case "warning":
+        iconComponent = ExclamationCircleFilled;
+        break;
+      case "error":
+      case "info":
+      default:
+        iconComponent = CloseCircleFilled;
+    }
+    return <Icon component={iconComponent} />;
+  }, [props.type]);
+
+  const customStyle = useMemo(() => {
+    const style: Interpolation<TTheme> = [alertStyle];
+    switch (props.type) {
+      case "success":
+        style.push(successAlertStyle);
+        break;
+      case "error":
+        style.push(errorAlertStyle);
+        break;
+      case "info":
+        style.push(infoAlertStyle);
+        break;
+      case "warning":
+        style.push(warningAlertStyle);
+        break;
+      default:
+        style.push(infoAlertStyle);
+    }
+
+    if (props.banner) {
+      style.push(alertBannerStyle);
+    }
+
+    return style;
+  }, [props.type, props.banner]);
+
+  const hasDescription = !!props.description;
+
+  const memoizedMessage = useMemo(
+    () =>
+      props.message ? <span css={hasDescription && boldTitleStyle}>{props.message}</span> : null,
+    [hasDescription, props.message]
+  );
+
+  const memoizedDescription = useMemo(
+    () => (props.description ? <span css={alertDescriptionStyle}>{props.description}</span> : null),
+    [props.description]
+  );
+
+  return (
+    <AntAlert
+      {...props}
+      css={customStyle}
+      icon={icon}
+      message={memoizedMessage}
+      description={memoizedDescription}
+    />
+  );
+};
+
+export default React.memo(Alert);
