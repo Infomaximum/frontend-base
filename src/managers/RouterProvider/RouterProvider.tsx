bea4e18a4a78c7bb9712b08b8c659ff32537e13d
Alexandr Petyaykin <a.petyaykin@infomaximum.com>
2023-01-22 15:40:20 +0300

    feat: перенес RouterProvider [IS-316]

---
+++ src/managers/RouterProvider/RouterProvider.tsx
@@ -0,0 +1,54 @@
+import { FC, useMemo } from "react";
+import {
+  getRoutes,
+  resolveConstraintsInRoutes,
+  routesMap,
+  sortPriority,
+} from "@im/base/src/utils/Routes/routes";
+import type { NCore } from "@im/core";
+import type { IRouterProviderProps } from "./RouterProvider.types";
+import { observer } from "mobx-react";
+import { useRoutes } from "react-router";
+import { RoutesContext } from "@im/base/src/decorators/contexts/RoutesContext";
+import { useFeature } from "@im/base/src/decorators/hooks/useFeature";
+
+const RouterProvider: FC<IRouterProviderProps> = ({
+  layout: LayoutProps,
+  isAuthorizedUser,
+  isSystemInitialized,
+  routesConfig,
+  unAuthorizedRoutes,
+  unInitializeRoutes,
+}) => {
+  const { isFeatureEnabled } = useFeature();
+
+  const resolvedRoutes = useMemo(() => {
+    return isFeatureEnabled
+      ? getRoutes(sortPriority(resolveConstraintsInRoutes(routesConfig, isFeatureEnabled)))
+      : null;
+  }, [routesConfig, isFeatureEnabled]);
+
+  let routesForRender: NCore.IRoutes[] = [];
+
+  if (!isSystemInitialized) {
+    routesForRender = routesMap(sortPriority(unInitializeRoutes));
+  } else if (!isAuthorizedUser) {
+    routesForRender = routesMap(sortPriority(unAuthorizedRoutes));
+  } else if (resolvedRoutes) {
+    routesForRender = resolvedRoutes;
+  }
+
+  const renderedRoutes = useRoutes(routesForRender);
+
+  if (isAuthorizedUser && resolvedRoutes) {
+    return (
+      <RoutesContext.Provider value={resolvedRoutes}>
+        <LayoutProps>{renderedRoutes}</LayoutProps>
+      </RoutesContext.Provider>
+    );
+  }
+
+  return renderedRoutes;
+};
+
+export default observer(RouterProvider);
