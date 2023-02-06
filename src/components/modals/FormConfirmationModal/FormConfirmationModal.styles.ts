593eb74e11bc7ce73d662a1fa7b67024c298d025
Alexandr Petyaykin <a.petyaykin@infomaximum.com>
2023-02-06 10:21:21 +0300

    feat: перенес FormConfirmationModal [IS-316]

---
+++ src/components/modals/FormConfirmationModal/FormConfirmationModal.styles.ts
@@ -0,0 +1,11 @@
+export const iconStyle = (theme: TTheme) => ({
+  fontSize: `${theme.h2FontSize}px`,
+  color: `${theme.gold6Color}`,
+  paddingRight: "16px",
+  position: "absolute" as const,
+});
+
+export const titleStyle = {
+  paddingLeft: "38px",
+  display: "inline-block",
+};
--- src/components/modals/index.ts
+++ src/components/modals/index.ts
@@ -2,3 +2,4 @@ export * from "./ConfirmationModal";
 export * from "./ErrorModal";
 export * from "./Modal";
 export * from "./RemoveConfirmationModal";
+export * from "./FormConfirmationModal";
