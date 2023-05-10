9446185f08c7ef8e654a85d59129df33eca3422c
Alexandr Petyaykin <saneksa2@gmail.com>
2023-05-10 12:04:31 +0300

    refactor: использован хук useLocalization в хоке withLoc

--- src/decorators/hocs/withLoc/withLoc.tsx
+++ src/decorators/hocs/withLoc/withLoc.tsx
@@ -1,18 +1,18 @@
 import type { TPropInjector } from "@infomaximum/utility";
-import { LocalizationContext } from "../../contexts/LocalizationContext";
 import hoistNonReactStatics from "hoist-non-react-statics";
 import type { Localization } from "@infomaximum/localization";
+import { useLocalization } from "../../hooks/useLocalization";
 
 export interface IWithLocProps {
   localization: Localization;
 }
 
 export const withLoc: TPropInjector<IWithLocProps> = (Component: any) => {
-  const LocalizedComponent = (props: any) => (
-    <LocalizationContext.Consumer>
-      {(localization: Localization) => <Component {...props} localization={localization} />}
-    </LocalizationContext.Consumer>
-  );
+  const LocalizedComponent = (props: any) => {
+    const localization = useLocalization();
+
+    return <Component {...props} localization={localization} />;
+  };
 
   return hoistNonReactStatics(LocalizedComponent, Component);
 };
