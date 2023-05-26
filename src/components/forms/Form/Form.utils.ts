492d2514fe09426fcc8948697ed6bdd0bf2e0869
Alexandr Petyaykin <saneksa2@gmail.com>
2023-05-26 15:02:27 +0300

    refactor: перенесены мутаторы формы в отдельный файл

--- src/components/forms/Form/Form.utils.ts
+++ src/components/forms/Form/Form.utils.ts
@@ -1,3 +1,8 @@
+import type { Mutator } from "final-form";
+import type { NCore } from "../../../libs/core";
+import { forEach } from "lodash";
+import type { TWrappedError } from "./FormWrapper.types";
+
 export type TOmittedConnectedFormProps<T extends Record<string, any>> = Omit<
   T,
   (typeof omittedPropsConnectedForm)[number]
@@ -42,3 +47,121 @@ export const omittedPropsConnectedForm = [
   "handleSubmit",
   "__versions",
 ] as const;
+
+// -------------------------------------------МУТАТОРЫ----------------------------------------------
+
+/**
+ * Мутатор изменения состояния touched полей формы
+ * @param fieldNames
+ */
+const touch: Mutator<unknown, Partial<Record<string, any>> | undefined> = (
+  [fieldNames]: [string[]],
+  state
+) => {
+  forEach(fieldNames, (fieldName) => {
+    const field = state.fields[fieldName];
+    if (field) {
+      field.touched = true;
+    }
+  });
+};
+
+/**
+ * Мутатор очистки полей формы
+ * @param fieldNames
+ */
+const clearFields: Mutator<unknown, Partial<Record<string, any>> | undefined> = (
+  [fieldNames]: [string[]],
+  state,
+  { changeValue }
+) => {
+  forEach(fieldNames, (fieldName) => {
+    const field = state.fields[fieldName];
+    if (field) {
+      changeValue(state, fieldName, () => undefined);
+    }
+  });
+};
+
+/**
+ * Мутатор ошибки формы
+ */
+const setSubmitError: Mutator<unknown, Partial<Record<string, any>> | undefined> = (
+  [error]: [NCore.TError],
+  state
+) => {
+  state.formState.submitError = error;
+};
+
+/**
+ * Очистка ошибки формы
+ * @param _
+ * @param state
+ */
+const clearSubmitError: Mutator<unknown, Partial<Record<string, any>> | undefined> = (_, state) => {
+  state.formState.submitError = null;
+};
+
+/**
+ * Мутатор ошибок формы
+ */
+const setSubmitErrors: Mutator<unknown, Partial<Record<string, any>> | undefined> = (
+  [fieldName, error]: [string, NCore.TError | TWrappedError[]],
+  state
+) => {
+  state.formState.submitErrors = {
+    ...state.formState.submitErrors,
+    [fieldName]: error,
+  };
+};
+
+/**
+ * Очистка ошибок формы
+ * @param _
+ * @param state
+ */
+const clearSubmitErrors: Mutator<unknown, Partial<Record<string, any>> | undefined> = (
+  _,
+  state
+) => {
+  state.formState.submitErrors = undefined;
+};
+
+/**
+ * Мутатор ошибки поля
+ * @param fieldName
+ */
+const setFieldError: Mutator<unknown, Partial<Record<string, any>> | undefined> = (
+  [fieldName, error]: [string, NCore.TError],
+  state
+) => {
+  if (fieldName && state?.formState?.errors) {
+    state.formState.errors[fieldName] = error;
+  }
+};
+
+/**
+ * Мутатор добавления элемента в начало массива
+ * @param fieldName
+ * @param value
+ */
+const unshift: Mutator<unknown, Partial<Record<string, any>> | undefined> = (
+  [fieldName, value]: [string, unknown],
+  state,
+  { changeValue }
+) => {
+  changeValue(state, fieldName, (lastValue = []) => {
+    return [value, ...lastValue];
+  });
+};
+
+export const formMutators = {
+  unshift,
+  touch,
+  clearFields,
+  setFieldError,
+  setSubmitError,
+  clearSubmitError,
+  setSubmitErrors,
+  clearSubmitErrors,
+};
--- src/components/forms/Form/FormWrapper.tsx
+++ src/components/forms/Form/FormWrapper.tsx
@@ -1,14 +1,12 @@
 import { Form as RFForm, type FormProps } from "react-final-form";
 import arrayMutators from "final-form-arrays";
-import type { Mutator } from "final-form";
-import type { NCore } from "@infomaximum/module-expander";
 import { useFeature } from "../../../decorators/hooks/useFeature";
 import { withFormSubmitPromise } from "../../../decorators";
-import type { IFormWrapperProps, TWrappedError } from "./FormWrapper.types";
+import type { IFormWrapperProps } from "./FormWrapper.types";
 import { type ComponentPropsWithoutRef, type FC, type PropsWithChildren, useMemo } from "react";
-import { forEach } from "lodash";
 import { FormComponent } from "./Form";
 import type { IBaseFormProps } from "../BaseForm/BaseForm.types";
+import { formMutators } from "./Form.utils";
 
 /**
  * Компонент-обертка над формой
@@ -25,121 +23,12 @@ const FormWrapper: FC<IFormWrapperProps> = ({
   const { isFeatureEnabled } = useFeature();
 
   const mutators = useMemo(() => {
-    /**
-     * Мутатор изменения состояния touched полей формы
-     * @param fieldNames
-     */
-    const touch: Mutator<unknown, typeof rest.initialValues> = (
-      [fieldNames]: [string[]],
-      state
-    ) => {
-      forEach(fieldNames, (fieldName) => {
-        const field = state.fields[fieldName];
-        if (field) {
-          field.touched = true;
-        }
-      });
-    };
-
-    /**
-     * Мутатор очистки полей формы
-     * @param fieldNames
-     */
-    const clearFields: Mutator<unknown, typeof rest.initialValues> = (
-      [fieldNames]: [string[]],
-      state,
-      { changeValue }
-    ) => {
-      forEach(fieldNames, (fieldName) => {
-        const field = state.fields[fieldName];
-        if (field) {
-          changeValue(state, fieldName, () => undefined);
-        }
-      });
-    };
-
-    /**
-     * Мутатор ошибки формы
-     */
-    const setSubmitError: Mutator<unknown, typeof rest.initialValues> = (
-      [error]: [NCore.TError],
-      state
-    ) => {
-      state.formState.submitError = error;
-    };
-
-    /**
-     * Очистка ошибки формы
-     * @param _
-     * @param state
-     */
-    const clearSubmitError: Mutator<unknown, typeof rest.initialValues> = (_, state) => {
-      state.formState.submitError = null;
-    };
-
-    /**
-     * Мутатор ошибок формы
-     */
-    const setSubmitErrors: Mutator<unknown, typeof rest.initialValues> = (
-      [fieldName, error]: [string, NCore.TError | TWrappedError[]],
-      state
-    ) => {
-      state.formState.submitErrors = {
-        ...state.formState.submitErrors,
-        [fieldName]: error,
-      };
-    };
-
-    /**
-     * Очистка ошибок формы
-     * @param _
-     * @param state
-     */
-    const clearSubmitErrors: Mutator<unknown, typeof rest.initialValues> = (_, state) => {
-      state.formState.submitErrors = undefined;
-    };
-
-    /**
-     * Мутатор ошибки поля
-     * @param fieldName
-     */
-    const setFieldError: Mutator<unknown, typeof rest.initialValues> = (
-      [fieldName, error]: [string, NCore.TError],
-      state
-    ) => {
-      if (fieldName && state?.formState?.errors) {
-        state.formState.errors[fieldName] = error;
-      }
-    };
-
-    /**
-     * Мутатор добавления элемента в начало массива
-     * @param fieldName
-     * @param value
-     */
-    const unshift: Mutator<unknown, typeof rest.initialValues> = (
-      [fieldName, value]: [string, unknown],
-      state,
-      { changeValue }
-    ) => {
-      changeValue(state, fieldName, (lastValue = []) => {
-        return [value, ...lastValue];
-      });
-    };
-
     return {
       ...mutatorsProp,
       ...arrayMutators,
-      unshift,
-      touch,
-      clearFields,
-      setFieldError,
-      setSubmitError,
-      clearSubmitError,
-      setSubmitErrors,
-      clearSubmitErrors,
+      ...formMutators,
     };
-  }, [mutatorsProp, rest]);
+  }, [mutatorsProp]);
 
   return (
     <RFForm<any>
