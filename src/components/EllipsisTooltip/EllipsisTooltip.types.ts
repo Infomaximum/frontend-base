145b08ff588d021b79050c35aa925172b4015069
Alexandr Petyaykin <saneksa2@gmail.com>
2024-01-17 09:04:24 +0300

    chore: удален EllipsisTooltip

--- src/components/EllipsisTooltip/EllipsisTooltip.types.ts
+++
@@ -1,6 +0,0 @@
-import type { Interpolation } from "@emotion/react";
-import type { TooltipPropsWithTitle } from "antd/lib/tooltip";
-
-export interface IEllipsisTooltipProps extends Partial<TooltipPropsWithTitle> {
-  customTextWrapperStyle?: Interpolation<TTheme>;
-}
--- src/components/EllipsisTooltip/index.ts
+++
@@ -1,1 +0,0 @@
-export { EllipsisTooltip } from "./EllipsisTooltip";
